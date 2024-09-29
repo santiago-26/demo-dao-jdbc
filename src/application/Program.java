5a595cfa587ee8e5c991b987e4dff3caab2f819f
Mauricio Santiago <mauriciosantiago95@hotmail.com>
2024-09-29 07:00:09 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,12 @@ public class Program {
 		for(Seller obj: list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 3: seller findAll====");
+		List<Seller> list2 = sellerDao.findAll();
+		for(Seller obj: list2) {
+			System.out.println(obj);
+		}
 	}
 
 }
