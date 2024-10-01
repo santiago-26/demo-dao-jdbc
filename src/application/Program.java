32af25fb46839b5ad723194ea26bc589ef44399e
Mauricio Santiago <mauriciosantiago95@hotmail.com>
2024-09-30 21:01:18 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,7 +29,13 @@ public class Program {
 		List<Seller> list2 = sellerDao.findAll();
 		for(Seller obj: list2) {
 			System.out.println(obj);
+			
 		}
+		
+		System.out.println("\n=== TEST 4: seller findAll====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(),4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new id = "+ newSeller.getId());
 	}
 
 }
